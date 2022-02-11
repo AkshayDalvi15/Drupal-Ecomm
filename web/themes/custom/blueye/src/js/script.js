fe4b294befea58f164dfcc79a68e6942e6cd1902
Akshay Dalvi <akshaydalvi212@gmail.com>
2022-02-11 20:34:53 +0530

    added js script to scroll up #10

---
+++ web/themes/custom/blueye/src/js/script.js
@@ -0,0 +1,15 @@
+const mybutton = document.querySelector("#myBtn");
+window.addEventListener("scroll",function ()  {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+  
+    }
+})
+
+
+mybutton.addEventListener("click",function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} )
