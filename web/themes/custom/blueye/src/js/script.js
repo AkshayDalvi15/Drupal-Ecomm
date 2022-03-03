b739198ccb744ca99298b1598aabea68cf813eff
Akshay Dalvi <akshaydalvi212@gmail.com>
2022-03-03 13:13:37 +0530

    updated script for the scroll to top function

--- web/themes/custom/blueye/src/js/script.js
+++ web/themes/custom/blueye/src/js/script.js
@@ -1,15 +1,36 @@
 const mybutton = document.querySelector("#myBtn");
-window.addEventListener("scroll",function ()  {
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-  
-    }
-})
+window.addEventListener("scroll", function () {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+});
 
-mybutton.addEventListener("click",function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-} )
+mybutton.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+const checkbox = document.getElementById("edit-policy");
+const subbtn = document.getElementById("edit-submit");
+const uname = document.getElementById("edit-name");
+const eemail = document.getElementById("edit-mail");
+// subbtn.disabled = true;
+
+// checkbox.addEventListener("change", function () {
+//   if (subbtn.disabled) {
+//     subbtn.disabled = false;
+//   } else {
+//     subbtn.disabled = true;
+//   }
+// });
+
+const conPass = document.getElementById("edit-pass-pass2");
+const Pass = document.getElementById("edit-pass-pass1");
+
+Pass.addEventListener("change", function () {
+  conPass.value = Pass.value;
+  uname.value = eemail.value;
+});
 
